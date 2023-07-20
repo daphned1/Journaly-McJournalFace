c92c32ff8e98aea78225930f68ce474c0dbb243e
Daphne <92963958+daphned1@users.noreply.github.com>
2023-07-19 20:36:25 -0700

    cleaned up the files. Made separatie class file for FlatFile.

---
+++ team-61-07/src/application/FlatFile.java
@@ -0,0 +1,143 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Class that handles finding, reading, and writing the password and security question/answer from an external file.
+ * The file name is JMJPassword.txt and contains the user password and security password.
+ * (implemented by Hayk/Richie)
+ */
+class FlatFile {
+	// this is a test of my git, sup guys :D
+	/**
+	 * Method that searches for a flat file.
+	 * @param name String input used for the file name
+	 * @return isFileFound Boolean to represent if the search is successful
+	 */
+	public boolean searchFile(String name) {
+		File f = new File(name + ".txt");
+		boolean isFileFound = f.isFile();
+		return isFileFound;
+	}
+	
+	/**
+	 * Method that creates an empty file through the writeFile method.
+	 * @param name String input used for the file name
+	 */
+	public void createEmptyFile(String name)  {
+		writeFile(name, "");
+	}
+	
+	/**
+	 * Method that creates a formatted array from the file contents for an account (password, security question, answer).
+	 * @param name String input used for the file name
+	 * @return accountDetails String array containing file contents for account
+	 */
+	public String[] readFileArray(String name) {
+		String[] accountDetails = new String[3];//initializing array
+		try {
+			Scanner readLines = new Scanner(new File(name + ".txt"));//Scanner to read file lines
+			accountDetails[0] = readLines.nextLine();//password
+			accountDetails[1] = readLines.nextLine();//security question
+			accountDetails[2] = readLines.nextLine();//answer
+			readLines.close();
+		} catch (IOException e) {
+			e.printStackTrace();//if the file does not exist
+		}
+		return accountDetails;
+	}
+	
+	/**
+	 * Method that writes content into an external flat file.
+	 * @param name String input used for the file name 
+	 * @param contents String input for contents to be written into the file
+	 */
+	public void writeFile(String name, String contents) {
+		try {
+			
+			/* commented out printFile usage
+			 * String current;
+			 * current = printFile(name);
+			*/
+			
+			FileWriter check = new FileWriter(name + ".txt", false);//override contents of file
+			BufferedWriter writer = new BufferedWriter(check);
+			/* commented out for the writing to override contents of file
+			if(current != null && current.equals("") == false) {
+				writer.write(current + "\n");
+			}
+			*/
+			writer.write(contents);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * Method that adds a line to a flatfile instead of replacing it
+	 * @param name String input used for the file name 
+	 * @param contents String input for contents to be written into the file
+	 */
+	public void addLineToFile(String name, String contents) {
+		try {
+			
+			//commented out printFile usage
+		 	String current;
+			current = printFile(name);
+			
+			
+			FileWriter check = new FileWriter(name + ".txt", false);//override contents of file
+			BufferedWriter writer = new BufferedWriter(check);
+			//commented out for the writing to override contents of file
+			if(current != null && current.equals("") == false) {
+				writer.write(current + "\n");
+			}
+			writer.write(contents);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void removeText(String name, String text) throws IOException {
+		String check = printFile(name);
+		String output = check.substring(0, check.indexOf(text)) + check.substring(check.indexOf(text)+text.length());;
+		writeFile(name, output);
+	}
+	/**
+	 * (currently unused) Method that prints the contents of a file into the program.
+	 * @param name String input used to find the file to print
+	 * @return output The content read by the program
+	 * @throws IOException
+	 */
+	public String printFile(String name) throws IOException {
+		String output = "";
+		String check;
+		try {
+			FileReader fReader = new FileReader(name + ".txt");
+			BufferedReader reader = new BufferedReader(fReader);
+			while((check = reader.readLine()) != null) {				
+				if(output.equals("")) {
+					output = output + check;
+				}
+				else {
+					output = output + "\n" + check;
+				}
+			}
+			reader.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return output;
+		
+	}
+	
+}
